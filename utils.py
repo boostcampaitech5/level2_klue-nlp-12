f503d2b2177e8902ea305cfcab24fe26c6fbd3c8
afterthougt <afterthougt@gmail.com>
2023-05-13 00:46:53 +0900

    refactor: migrate wandb functions to utils module

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
+import logging
 import os
 import random
-import logging
 
-import torch
 import numpy as np
+import torch
+import wandb
+from wandb import AlertLevel
 
 log = logging.getLogger(__name__)
 
@@ -18,3 +20,21 @@ def seed_everything(seed, workers: bool = False) -> int:
 
     os.environ["PL_SEED_WORKERS"] = f"{int(workers)}"
     return seed
+
+
+def init_wandb(config, run_name):
+    if not config.use_wandb:
+        return
+
+    wandb.init(
+        entity=config.wandb['entity'],
+        project=config.wandb['project_name'],
+        name=run_name,
+        config=config,
+    )
+    wandb.alert(title='start', level=AlertLevel.INFO, text=f'{run_name}')
+
+
+def alert_wandb(config, run_name, title):
+    if config.use_wandb:
+        wandb.alert(title='finished', level=AlertLevel.INFO, text=f'{run_name}')
