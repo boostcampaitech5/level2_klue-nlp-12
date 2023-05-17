7d3cebc44f1f0abf5b5a575b778d5ac5c491776a
afterthougt <afterthougt@gmail.com>
2023-05-17 16:55:59 +0900

    feat: remove batch norm and dropout and add bi-rnn initialization

--- model.py
+++ model.py
@@ -53,18 +53,25 @@ class BiGRUREModel(nn.Module):
             self.plm.resize_token_embeddings(new_num_tokens)
 
         self.hidden_size = self.model_config.hidden_size  # 1024 for roberta-large
-        # TODO: implement initialization of gru, classifier
         self.gru = nn.GRU(input_size=self.hidden_size,
                           hidden_size=self.hidden_size,
                           num_layers=1,
-                          dropout=0.1,
                           batch_first=True,  # (bsz, seq, feature) if True else (seq, bsz, feature)
                           bidirectional=True)
-        self.bn = nn.BatchNorm1d(self.hidden_size * 2)
+        self.init_gru()
         self.gelu = nn.GELU()
-        self.dropout = nn.Dropout(0.1)
         self.classifier = nn.Linear(self.hidden_size * 2, config.num_labels)
         nn.init.kaiming_normal_(self.classifier.weight, mode='fan_in', nonlinearity='relu')
+        self.classifier.bias.data.fill_(0)
+
+    def init_gru(self):
+        for name, param in self.gru.named_parameters():
+            if 'weight_ih' in name:
+                nn.init.xavier_normal_(param.data)
+            elif 'weight_hh' in name:
+                nn.init.xavier_normal_(param.data)
+            elif 'bias' in name:
+                param.data.fill_(0)
 
     def forward(self, input_ids: Tensor, token_type_ids: Tensor, attention_mask, labels=None):
         outputs = self.plm(input_ids=input_ids,
@@ -72,9 +79,7 @@ class BiGRUREModel(nn.Module):
                            attention_mask=attention_mask).last_hidden_state
         _, next_hidden = self.gru(outputs)
         outputs = torch.cat([next_hidden[0], next_hidden[1]], dim=1)
-        outputs = self.bn(outputs)
         outputs = self.gelu(outputs)
-        outputs = self.dropout(outputs)
         logits = self.classifier(outputs)
         return {
             'logits': logits,
@@ -95,18 +100,25 @@ class BiLSTMREModel(nn.Module):
             self.plm.resize_token_embeddings(new_num_tokens)
 
         self.hidden_size = self.model_config.hidden_size  # 1024 for roberta-large
-        # TODO: implement initialization of lstm, classifier
         self.lstm = nn.LSTM(input_size=self.hidden_size,
                             hidden_size=self.hidden_size,
                             num_layers=1,
-                            dropout=0.1,
                             batch_first=True,  # (bsz, seq, feature) if True else (seq, bsz, feature)
                             bidirectional=True)
-        self.bn = nn.BatchNorm1d(self.hidden_size * 2)
+        self.init_lstm()
         self.gelu = nn.GELU()
-        self.dropout = nn.Dropout(0.1)
         self.classifier = nn.Linear(self.hidden_size * 2, config.num_labels)
         nn.init.kaiming_normal_(self.classifier.weight, mode='fan_in', nonlinearity='relu')
+        self.classifier.bias.data.fill_(0)
+
+    def init_lstm(self):
+        for name, param in self.lstm.named_parameters():
+            if 'weight_ih' in name:
+                nn.init.xavier_normal_(param.data)
+            elif 'weight_hh' in name:
+                nn.init.xavier_normal_(param.data)
+            elif 'bias' in name:
+                param.data.fill_(0)
 
     def forward(self, input_ids: Tensor, token_type_ids: Tensor, attention_mask, labels=None):
         outputs = self.plm(input_ids=input_ids,
@@ -114,9 +126,7 @@ class BiLSTMREModel(nn.Module):
                            attention_mask=attention_mask).last_hidden_state
         _, (next_hidden, _) = self.lstm(outputs)
         outputs = torch.cat([next_hidden[0], next_hidden[1]], dim=1)
-        outputs = self.bn(outputs)
         outputs = self.gelu(outputs)
-        outputs = self.dropout(outputs)
         logits = self.classifier(outputs)
         return {
             'logits': logits,
