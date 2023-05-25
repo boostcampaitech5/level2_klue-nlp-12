c1d1202ba0b14fb65b3fd50899bb6b4d7eabd275
ohilikeit <yjh091500@naver.com>
2023-05-25 03:40:48 +0000

    feat: add CustomModel for entity embedding layer

--- model/model.py
+++ model/model.py
@@ -38,7 +38,63 @@ class BaseREModel(nn.Module):
             'logits': logits,
         }
 
+class CustomModel(nn.Module):
+    def __init__(self, config, new_num_tokens: int):
+        super().__init__()
 
+        self.model_config = AutoConfig.from_pretrained(config.model['name'])
+        self.model_config.num_labels = config.num_labels
+        
+        self.plm = AutoModelForSequenceClassification.from_pretrained(config.model['name'],
+                                                                      config=self.model_config)
+
+        if self.model_config.vocab_size != new_num_tokens:
+            self.plm.resize_token_embeddings(new_num_tokens)
+        
+        self.hidden_size = self.model_config.hidden_size
+        
+        self.entity_embedding = nn.Embedding(3, self.hidden_size)
+        nn.init.xavier_normal_(self.entity_embedding.weight)
+
+        self.weight = nn.Parameter(torch.Tensor(1))  # Learnable weight parameter
+        nn.init.uniform_(self.weight)
+
+        # self.reduction_layer = nn.Linear(self.hidden_size * 2, self.hidden_size)
+
+    @autocast()
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        attention_mask=None,
+        entity_ids=None,
+        labels=None,
+    ):
+        # entity_ids = entity_ids.long()
+        entity_embeddings = self.entity_embedding(entity_ids)          # torch.tensor([64, 180, 1024])
+        input_embeddings = self.plm.get_input_embeddings()(input_ids)  # torch.tensor([64, 180, 1024])
+
+        # 단순히 더한 버전
+        # combined_embeddings = input_embeddings + entity_embeddings
+
+        # concat 버전
+        # combined_embeddings = torch.cat([input_embeddings,entity_embeddings], dim=-1) # torch.tensor([64, 180, 2048])
+        # combined_embeddings = self.reduction_layer(combined_embeddings) # torch.tensor([64, 180, 1024])
+
+        # weighted sum 버전
+        combined_embeddings = self.weight * input_embeddings + (1 - self.weight) * entity_embeddings
+
+        outputs = self.plm.roberta(
+            inputs_embeds=combined_embeddings,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+        logits = self.plm.classifier(outputs['last_hidden_state'])
+
+        return {
+            'logits': logits,
+        }
+    
 class BiGRUREModel(nn.Module):
     """
     Pre-trained Language Model로부터 나온 logits를 Bi-driectional GRU에 통과시킨 후
